f6fc6eed8d7603d3cc4f181f7229267e23bbbc00
VarmiloVA <varmilo.blue@protonmail.com>
2024-10-30 09:23:25 +0100

    Petición de datos en p2b correcta

--- FP/FP2/FP_P2B_G40/main.c
+++ FP/FP2/FP_P2B_G40/main.c
@@ -17,7 +17,27 @@ int main()
     int rg1, rg2;
 
     //Pide los valores del rango
-    printf("Introduce el número mínimo de rango: ")
+    do{
+        printf("Introduce el numero minimo del rango: ");
+        scanf("%d", &rg1);
+        if (rg1<0) {
+            printf("El numero minimo del rango no puede ser menor que 0\n");
+        };
+    } while (rg1<0);
+    do{
+        printf("Introduce el numero maximo del rango: ");
+        scanf("%d", &rg2);
+        if (rg2<=rg1) {
+            printf("El numero maximo del rango no puede ser menor/igual que el minimo");
+        };
+    } while (rg2<=rg1);
+
+    //Test
+    printf("rg1 es %d\n", rg1);
+    printf("rg2 es %d", rg2);
+
+
+
     //
     return 0;
 }
