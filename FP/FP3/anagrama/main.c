7bfb561de00574f57426ad0451230711a486281c
azerby-ra <raulsanztome@gmail.com>
2024-11-25 11:34:00 +0100

    Tengo cambios que no se cuales son

--- FP/FP3/anagrama/main.c
+++ FP/FP3/anagrama/main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 /***********************************************************
-    Programa: anagrama
+    subPrograma: anagrama
     Función: recoge 2 palabras y comprueba si son anagramas
              devolviendo si lo son o no por pantalla.
 ***********************************************************/
@@ -11,19 +11,41 @@
 int main()
 {
     char word1[50], word2[50];
-    int stop, i, e;
+    int stop, i, e, j, k;
 
     //Hacer que si no tienen la misma longitud o si es la misma palabra
     //vuelva a pedir introducirlas.
-    printf("\nIntroduce la palabra 1: ");
-    fgets(word1, sizeof(word1), stdin);
+    do{
+        printf("\nIntroduce la palabra 1: ");
+        fgets(word1, sizeof(word1), stdin);
 
-    printf("\nIntroduce la palabra 2: ");
-    fgets(word2, sizeof(word2), stdin);
+        word1[strcspn(word1, "\n")] = '\0';
 
-    for (int i = 0; i <= sizeof(word1); i++) {
+        printf("\nIntroduce la palabra 2: ");
+        fgets(word2, sizeof(word2), stdin);
 
-    }
+        word2[strcspn(word2, "\n")] = '\0';
+
+        printf("%s, %s", word1, word2);
+
+        for (i = 0; i <= sizeof(word1); i++) {
+            e = word1[i];
+            printf("\%d", e);
+            if (e = '\0'){
+                break;
+            }
+        }
+        printf("%d", i);
+
+        for (j = 0; j <= sizeof(word2); j++) {
+            k = word2[j];
+            printf("\%d", k);
+            if (k = '\0'){
+                break;
+            }
+        }
+        printf("%d", j);
+    }while(i != j);
 
     return 0;
 }
