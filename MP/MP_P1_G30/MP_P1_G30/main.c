7b14e00ceb837d9e0033ce34118bcc6de1d8fd71
azerby-ra <raulsanztome@gmail.com>
2025-02-24 16:13:53 +0100

    Nueva carpeta

---
+++ MP/MP_P1_G30/MP_P1_G30/main.c
@@ -0,0 +1,19 @@
+/*  Asigantura: Metodología de la programación Curso: 1
+    Tarea que realiza el programa: Gestión de clientes, habitaciones y reservas de un hotel
+    Alumno 1: Atanasov Angelov, Daniel
+    Alumno 2: Sanz Tomé, Raúl
+    Grupo de laboratorio: 30
+    Fecha: 24/02/2025 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "spanish");
+    printf("¡Bienvenido a la aplicación GEST_HOTEL!\n");
+    system("pause");
+    system("cls");
+    return 0;
+}
