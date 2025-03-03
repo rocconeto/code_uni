413438dd096555d2d1f849b0901388eb4533bd99
VarmiloVA <varmilo.blue@protonmail.com>
2025-03-03 12:55:31 +0100

    Creada carpeta con scripts últiles para diferentes ejercicios

---
+++ scripts/crear_listas.py
@@ -0,0 +1,6 @@
+print("Script creado inicialmente para poder formar una lista con los datos de alturas en estadistica/P1/Practica1")
+datos = input("Pegar datos: ")
+
+lista_datos = datos.split(" ")
+
+print(lista_datos)
